31917d9c768e5d387da611af59535fab87304d73
Heba Imad <hibaemad2024@gmail.com>
2026-06-18 00:21:05 +0300

    Fix Railway port and MySQL

--- backend/server.js
+++ backend/server.js
@@ -259,6 +259,8 @@ console.log("Blood requests route called");
   );
 
 });
-app.listen(5000, () => {
-  console.log("Server started on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
