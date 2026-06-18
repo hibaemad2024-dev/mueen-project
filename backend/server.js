d0db7cb8062ca58bd21cfc99d0b3ea77912ea1ba
Heba Imad <hibaemad2024@gmail.com>
2026-06-18 19:27:14 +0300

    Fix MySQL database variable

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,7 @@ const db = mysql.createConnection({
   host: process.env.MYSQLHOST,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
+  database: process.env.MYSQL_DATABASE,
   port: process.env.MYSQLPORT,
   charset: "utf8mb4_unicode_ci"
 });
